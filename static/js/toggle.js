6c0f1346200d1ac495bcecdb4bb5bc72c57cfa11
Koha <BelkoZdorovo@yandex.ru>
2024-03-11 12:44:39 +0300

    Portfolio filled

--- static/js/toggle.js
+++ static/js/toggle.js
@@ -2,7 +2,18 @@ function toggleInfo(element) {
     var infoContent = element.nextElementSibling;
     if (infoContent.style.display === 'none') {
         infoContent.style.display = 'block';
+        document.body.style.height = 'auto';
     } else {
         infoContent.style.display = 'none';
+        document.body.style.height = '100%';
     }
+}
+
+window.onscroll = function() {
+  var top = document.querySelector('.top');
+  if (window.scrollY > 50) {
+    top.style.backgroundColor = 'rgba(55, 112, 201, 0.9)'; /* Сделать блок top чуть менее прозрачным */
+  } else {
+    top.style.backgroundColor = 'rgb(255, 255, 255, 0)'; /* Сделать блок top полностью непрозрачным */
+  }
 }
